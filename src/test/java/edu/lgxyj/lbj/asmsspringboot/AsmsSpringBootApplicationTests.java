f4ada585c6b6425b824acfe6cc1d02d1219cebfd
Jieuol <356299864@qq.com>
2024-03-04 08:45:44 +0800

    3.4 first commit

---
+++ src/test/java/edu/lgxyj/lbj/asmsspringboot/AsmsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lgxyj.lbj.asmsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
