4753ce6fe6a3c8f02a762b6015cbb94a8a01d017
Jieuol <356299864@qq.com>
2024-03-17 19:38:40 +0800

    test vue

--- src/router/index.js
+++ src/router/index.js
@@ -56,15 +56,6 @@ const routes = [
 					title: '修改个人信息'
 				},
 			},
-			// {
-			//     path: '/userInfo',
-			//     name: 'userInfo', 
-			//     component: () => import("@/views/userInfo.vue"),
-			// 	meta: {
-			// 		index: 0,
-			// 		title: '修改个人信息'
-			// 	},
-			// },
 			{
 			    path: '/notice',
 			    name: 'notice', 
